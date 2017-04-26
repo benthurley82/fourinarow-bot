afe7534975223cf0dbb77e9503671361147e98b3
Ben Thurley <ben.thurley@coraltreesystems.com>
2017-04-26 17:34:16 +0100

    Add unit test for bad move
    This is passing already with the latest version so no game engine
    changes required

--- src/test/java/bot/BadMovesTest.java
+++ src/test/java/bot/BadMovesTest.java
@@ -1,6 +1,7 @@
 package bot;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
 
@@ -39,6 +40,20 @@ public class BadMovesTest
 		System.out.println(move);
 
 		assertEquals("Need to go in column 2 to win", 2, move);
+	}
+
+	@Test
+	public void testMove3()
+	{
+		Field field = new Field(7, 6);
+		field.parseFromString(
+				"0,0,0,0,0,0,0;0,0,0,0,0,0,0;0,0,0,0,0,0,0;0,0,0,2,0,0,0;0,0,1,1,1,0,0;0,0,2,1,2,0,2");
+		field.printField();
+		int move = mover.makeTurn(9, field, 1);
+		System.out.println(move);
+
+		assertNotEquals("Avoid column 1 giving the win away", 1, move);
+		assertNotEquals("Avoid column 5 giving the win away", 5, move);
 	}
 
 }
