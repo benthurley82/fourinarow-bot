bb9aa08a5eecea0a873685e9355db39cafc05b50
Ben Thurley <ben.thurley@coraltreesystems.com>
2017-04-30 12:13:34 +0100

    Add batch of real game losses

--- src/test/java/bot/GameLossesTest.java
+++ src/test/java/bot/GameLossesTest.java
@@ -1,6 +1,7 @@
 package bot;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
 
@@ -56,6 +57,131 @@ public class GameLossesTest
 		System.out.println(move);
 
 		assertEquals("Need to go in column 1 to setup the win", 1, move);
+	}
+
+	@Test
+	public void testLoss4()
+	{
+		Field field = new Field(7, 6);
+		field.parseFromString(
+				"0,0,0,0,0,0,0;0,0,0,2,0,0,0;1,0,1,1,0,0,0;2,0,2,1,1,0,0;1,0,1,2,2,0,0;1,0,2,1,2,2,0");
+		field.printField();
+		int move = mover.makeTurn(18, field, 2);
+		System.out.println(move);
+
+		assertEquals("Need to go in column 5 to setup a win and block opponent",
+				5, move);
+	}
+
+	@Test
+	public void testLoss5()
+	{
+		Field field = new Field(7, 6);
+		field.parseFromString(
+				"0,0,0,0,0,0,0;0,0,0,0,2,0,0;0,1,0,2,1,0,0;0,2,0,1,2,0,0;0,2,1,2,1,2,0;0,2,1,1,2,1,1");
+		field.printField();
+		int move = mover.makeTurn(19, field, 1);
+		System.out.println(move);
+
+		assertNotEquals("Don't go in column 2 which is easily blocked", 2,
+				move);
+	}
+
+	@Test
+	public void testLoss6()
+	{
+		Field field = new Field(7, 6);
+		field.parseFromString(
+				"0,0,0,2,1,0,0;0,2,0,1,1,0,0;0,1,0,1,1,1,0;0,2,0,1,2,2,0;0,2,1,2,1,1,0;0,2,2,1,2,2,0");
+		field.printField();
+		int move = mover.makeTurn(24, field, 2);
+		System.out.println(move);
+
+		assertNotEquals("Don't go in column 2 which allows opponent to win", 2,
+				move);
+		assertNotEquals("Don't go in column 5 which allows opponent to win", 5,
+				move);
+	}
+
+	@Test
+	public void testLoss7()
+	{
+		Field field = new Field(7, 6);
+		field.parseFromString(
+				"0,0,1,2,1,0,0;0,0,2,2,2,0,0;1,0,2,1,2,0,1;2,0,2,1,1,0,2;1,0,1,2,2,0,1;1,2,1,1,2,0,1");
+		field.printField();
+		int move = mover.makeTurn(28, field, 2);
+		System.out.println(move);
+
+		assertNotEquals("Don't go in column 1 which allows opponent to win", 1,
+				move);
+	}
+
+	@Test
+	public void testLoss8()
+	{
+		Field field = new Field(7, 6);
+		field.parseFromString(
+				"0,0,2,2,0,0,0;0,0,1,2,0,2,0;2,0,2,2,0,1,0;1,0,1,1,0,2,0;1,0,1,2,1,1,0;2,1,1,1,2,2,0");
+		field.printField();
+		int move = mover.makeTurn(25, field, 1);
+		System.out.println(move);
+
+		assertNotEquals("Don't go in column 1 which allows opponent to win", 1,
+				move);
+	}
+
+	@Test
+	public void testLoss9()
+	{
+		Field field = new Field(7, 6);
+		field.parseFromString(
+				"0,0,0,0,0,0,0;0,0,0,0,0,0,0;0,0,2,1,0,0,0;0,0,2,2,0,0,0;0,0,1,1,0,0,0;0,0,2,1,0,0,0");
+		field.printField();
+		int move = mover.makeTurn(9, field, 1);
+		System.out.println(move);
+
+		assertNotEquals("Don't go in column 0 which allows opponent to win", 0,
+				move);
+		assertNotEquals("Don't go in column 1 which allows opponent to win", 1,
+				move);
+		assertNotEquals("Don't go in column 3 which allows opponent to win", 3,
+				move);
+		assertNotEquals("Don't go in column 6 which allows opponent to win", 6,
+				move);
+	}
+
+	@Test
+	public void testLoss10()
+	{
+		Field field = new Field(7, 6);
+		field.parseFromString(
+				"0,0,0,0,0,0,0;0,0,0,2,0,0,0;1,0,1,1,0,0,0;2,0,2,1,1,0,0;1,0,1,2,2,0,0;1,0,2,1,2,2,0");
+		field.printField();
+		int move = mover.makeTurn(18, field, 2);
+		System.out.println(move);
+
+		assertNotEquals("Don't go in column 0 which allows opponent to win", 0,
+				move);
+		assertNotEquals("Don't go in column 1 which allows opponent to win", 1,
+				move);
+		assertNotEquals("Don't go in column 3 which allows opponent to win", 3,
+				move);
+		assertNotEquals("Don't go in column 4 which allows opponent to win", 4,
+				move);
+	}
+
+	@Test
+	public void testLoss11()
+	{
+		Field field = new Field(7, 6);
+		field.parseFromString(
+				"0,0,0,0,0,0,0;0,0,0,2,0,0,0;0,0,0,1,0,0,0;0,0,0,1,0,0,0;1,0,0,2,0,0,0;1,0,2,1,2,0,0");
+		field.printField();
+		int move = mover.makeTurn(10, field, 2);
+		System.out.println(move);
+
+		assertEquals("Need to go in column 4", 4, move);
 	}
 
 }
